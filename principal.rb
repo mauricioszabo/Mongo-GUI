59867fdf98d5f389367324d0c711114c5ee2a517
Maurício Szabo <mauricio.szabo@gmail.com>
2009-12-01 17:06:00 +0000

    Suporte (primitivo) à buscas.

--- principal.rb
+++ principal.rb
@@ -3,6 +3,10 @@ require 'Qt4'
 require 'buscar_lazy'
 require 'ui/principal'
 
+def __(string, classe = '')
+  return Qt::Application.translate('', string, nil, Qt::Application::UnicodeUTF8)
+end
+
 class Principal < Qt::MainWindow
   def initialize
     super
@@ -25,6 +29,23 @@ class Principal < Qt::MainWindow
 
     @ui.anterior.connect( SIGNAL('clicked()') ) { mudar_elemento -1 }
     @ui.proximo.connect( SIGNAL('clicked()') ) { mudar_elemento 1 }
+
+    @ui.consultar.connect( SIGNAL('clicked()') ) { consultar(@ui.consulta.text) }
+  end
+
+  def consultar(texto)
+    texto.strip!
+    texto = "{#{texto}}" unless texto[0] == ?{
+    begin
+      hash = eval(texto)
+    rescue Exception => e
+      Qt::MessageBox.critical self, 'Erro de sintaxe', "Ocorreu um erro de sintaxe em sua busca: #{e.message}"
+    end
+
+    @elementos = BuscarLazy.new(@collection, hash)
+    @ui.dados_documento.clear
+    popular_arvore(@ui.dados_documento, nil, @elementos[0])
+    @ui.contador.text = @elementos.msg
   end
 
   def mudar_elemento(contador)
@@ -49,7 +70,6 @@ class Principal < Qt::MainWindow
 
     @ui.dados_documento.clear
     popular_arvore(@ui.dados_documento, nil, @elementos[0])
-
     @ui.contador.text = @elementos.msg
   end
 
@@ -57,17 +77,17 @@ class Principal < Qt::MainWindow
   def popular_arvore(pai, chave, elemento)
     case(elemento)
       when Hash, OrderedHash
+        e = Qt::TreeWidgetItem.new(pai, [__(chave), "{}"]) unless chave.nil?
         elemento.each_pair do |ch, valor|
-          e = Qt::TreeWidgetItem.new(pai, [chave, "{}"]) unless chave.nil?
           popular_arvore(e || pai, ch, valor)
         end
       when Array
         e = Qt::TreeWidgetItem.new(pai, [chave, "[]"])
         elemento.each_with_index do |valor, indice|
-          popular_arvore(e, indice.to_s, valor)
+          popular_arvore(e, indice.to_s, __(valor))
         end
       else
-        Qt::TreeWidgetItem.new(pai, [chave, elemento.to_s])
+        Qt::TreeWidgetItem.new(pai, [__(chave), __(elemento.to_s)])
     end
   end
 end
@@ -75,6 +95,7 @@ end
 if $0 == __FILE__
   app = Qt::Application.new ARGV
 
+  Qt::TextCodec.codec_for_tr = Qt::TextCodec.codec_for_name("UTF-8")
   Qt::TextCodec::setCodecForTr(Qt::TextCodec::codecForName("UTF-8"));
   Qt::TextCodec::setCodecForCStrings(Qt::TextCodec::codecForName("UTF-8"));
   Qt::TextCodec::setCodecForLocale(Qt::TextCodec::codecForName("UTF-8"));
